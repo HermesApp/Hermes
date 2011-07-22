4f31f66be8ed1d5e6a13699976ec7cbc6d409dfe
Alex Crichton <alex@alexcrichton.com>
2011-07-22 00:58:17 -0700

    Update Keychain.{m,h} with more sane method of storing/retrieving passwords

--- Classes/Keychain.h
+++ Classes/Keychain.h
@@ -1,3 +1,7 @@
 
+#pragma once
+
+#define KEYCHAIN_SERVICE_NAME "Hermes"
+
 BOOL KeychainSetItem(NSString* username, NSString *password);
 NSString* KeychainGetPassword(NSString *username);
--- Classes/Keychain.m
+++ Classes/Keychain.m
@@ -2,125 +2,40 @@
 #import "Keychain.h"
 
 #import <Security/Security.h>
-#import <CoreFoundation/CoreFoundation.h>
-
-
-void KeychaininitializeAttributes(SecKeychainAttribute* attributes, NSString *username) {
-  attributes[0].tag    = kSecGenericItemAttr;
-  attributes[0].data   = KEYCHAIN_GENERIC_ATTR;
-  attributes[0].length = sizeof(KEYCHAIN_GENERIC_ATTR);
-
-  attributes[1].tag    = kSecLabelItemAttr;
-  attributes[1].data   = KEYCHAIN_LABEL_ATTR;
-  attributes[1].length = sizeof(KEYCHAIN_LABEL_ATTR);
-
-  attributes[2].tag    = kSecAccountItemAttr;
-  attributes[2].data   = (void*) [username cStringUsingEncoding:NSUTF8StringEncoding];
-  attributes[2].length = [username length];
-}
-
-SecKeychainItemRef KeychainItemFor(NSString* username) {
-  SecKeychainSearchRef search;
-  SecKeychainItemRef item = NULL;
-  SecKeychainAttribute attributes[3];
-  OSErr result;
-
-  KeychaininitializeAttributes(attributes, username);
-
-  SecKeychainAttributeList list = {3, attributes};
-
-  result = SecKeychainSearchCreateFromAttributes(NULL,
-      kSecGenericPasswordItemClass, &list, &search);
-
-  if (result == noErr) {
-    result = SecKeychainSearchCopyNext(search, &item);
-
-    if (result != noErr) {
-      item = NULL;
-    }
-  }
-
-  if (search)
-    CFRelease(search);
-
-  return item;
-}
-
-SecKeychainItemRef KeychainCreateItemFor(NSString* username, NSString* password) {
-  SecKeychainAttribute attributes[3];
-  SecKeychainAttributeList list;
-  SecKeychainItemRef item = NULL;
-  OSStatus status;
-
-  KeychaininitializeAttributes(attributes, username);
-
-  list.count = 3;
-  list.attr  = attributes;
-
-  status = SecKeychainItemCreateFromContent(kSecGenericPasswordItemClass, &list,
-    [password length], [password cStringUsingEncoding:NSUTF8StringEncoding],
-    NULL, NULL, &item);
-
-  if (status != 0) {
-    item = NULL;
-  }
-
-  return item;
-}
 
 BOOL KeychainSetItem(NSString* username, NSString* password) {
-  OSStatus status;
-  BOOL ret = NO;
-  SecKeychainAttribute attributes[1];
-
-  attributes[0].tag    = kSecAccountItemAttr;
-  attributes[0].data   = (void*) [username cStringUsingEncoding:NSUTF8StringEncoding];
-  attributes[0].length = [username length];
-
-  SecKeychainAttributeList list = {1, attributes};
-
-  SecKeychainItemRef item = KeychainItemFor(username);
-  if (item == NULL) {
-    item = KeychainCreateItemFor(username, password);
-    ret = (item != NULL);
-
-    if (ret) {
-      CFRelease(item);
-    }
-
-    return ret;
-  }
-
-  status = SecKeychainItemModifyContent(item, &list, [password length],
-                          [password cStringUsingEncoding:NSUTF8StringEncoding]);
-
-  ret = (status == noErr);
-
-  CFRelease(item);
-  return ret;
+  return noErr == SecKeychainAddGenericPassword(
+    NULL,
+    strlen(KEYCHAIN_SERVICE_NAME),
+    KEYCHAIN_SERVICE_NAME,
+    [username length],
+    [username UTF8String],
+    [password length],
+    [password UTF8String],
+    NULL);
 }
 
 NSString *KeychainGetPassword(NSString* username) {
+  void *password = NULL;
   UInt32 length;
-  char *password;
-  OSStatus status;
-  NSString *ret = nil;
+  OSStatus result = SecKeychainFindGenericPassword(
+    NULL,
+    strlen(KEYCHAIN_SERVICE_NAME),
+    KEYCHAIN_SERVICE_NAME,
+    [username length],
+    [username UTF8String],
+    &length,
+    &password,
+    NULL);
 
-  SecKeychainItemRef item = KeychainItemFor(username);
-  if (item == NULL) {
+  if (result != noErr) {
+    SecKeychainItemFreeContent(NULL, password);
     return nil;
   }
 
-  status = SecKeychainItemCopyContent (item, NULL, NULL, &length,
-    (void**)&password);
-
-  if (status == noErr) {
-    ret = [[NSString alloc] initWithBytes:password
-        length:length encoding:NSUTF8StringEncoding];
-    [ret autorelease];
-    SecKeychainItemFreeContent(NULL, password);
-  }
-
-  CFRelease(item);
+  NSString *ret = [[NSString alloc] initWithBytes:password length:length encoding:NSUTF8StringEncoding];
+  SecKeychainItemFreeContent(NULL, password);
+  [ret autorelease];
+  
   return ret;
 }
