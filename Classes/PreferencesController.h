29a6d8697f5277e1de7fe53c72a746e0e2e31bdf
Alex Crichton <alex@alexcrichton.com>
2012-06-06 23:12:27 -0700

    Remove all unnecessary preferences code

--- Classes/PreferencesController.h
+++ Classes/PreferencesController.h
@@ -4,56 +4,41 @@
  *        which are set from it
  */
 
-#define PLEASE_BIND_MEDIA     @"hermes.please-bind-media"
-#define PLEASE_SCROBBLE       @"hermes.please-scrobble"
-#define PLEASE_SCROBBLE_LIKES @"hermes.please-scrobble-likes"
-#define ONLY_SCROBBLE_LIKED   @"hermes.only-scrobble-liked"
-#define PLEASE_GROWL          @"hermes.please-growl"
-#define PLEASE_GROWL_NEW      @"hermes.please-growl-new"
-#define PLEASE_GROWL_PLAY     @"hermes.please-growl-play"
-#define PLEASE_CLOSE_DRAWER   @"hermes.please-close-drawer"
-#define DRAWER_WIDTH          @"hermes.drawer-width"
-#define DESIRED_QUALITY       @"hermes.audio-quality"
-#define LAST_PREF_PANE        @"hermes.last-pref-pane"
+/* If these are changed, then the xib also needs to be updated */
+#define PLEASE_BIND_MEDIA     @"pleaseBindMedia"
+#define PLEASE_SCROBBLE       @"pleaseScrobble"
+#define PLEASE_SCROBBLE_LIKES @"pleaseScrobbleLikes"
+#define ONLY_SCROBBLE_LIKED   @"onlyScrobbleLiked"
+#define PLEASE_GROWL          @"pleaseGrowl"
+#define PLEASE_GROWL_NEW      @"pleaseGrowlNew"
+#define PLEASE_GROWL_PLAY     @"pleaseGrowlPlay"
+#define PLEASE_CLOSE_DRAWER   @"pleaseCloseDrawer"
+#define DRAWER_WIDTH          @"drawerWidth"
+#define DESIRED_QUALITY       @"audioQuality"
+#define LAST_PREF_PANE        @"lastPrefPane"
 
-#define QUALITY_HIGH @"high"
-#define QUALITY_LOW  @"low"
-#define QUALITY_MED  @"medium"
+/* If observing a value, then the method which is implemented is:
+   observeValueForKeyPath:(NSString*) ofObject:(id) change:(NSDictionary*)
+                  context:(void*) */
+#define PREF_PATH(name) (@"values." name)
+#define PREF_CONTROLLER [NSUserDefaultsController sharedUserDefaultsController]
+#define PREF_OBSERVE_VALUE(x, y) [PREF_CONTROLLER addObserver:(x)              \
+    forKeyPath:PREF_PATH(y)  options:NSKeyValueObservingOptionOld context:nil];
+#define PREF_UNOBSERVE_VALUES(x, y) [PREF_CONTROLLER removeObserver:(x)        \
+    forKeyPath:PREF_PATH(y) context:nil]
+#define PREF_KEY_VALUE(x) [[PREF_CONTROLLER values] valueForKey:(x)]
+#define PREF_KEY_BOOL(x) [(PREF_KEY_VALUE(x)) boolValue]
+
+#define QUALITY_HIGH 0
+#define QUALITY_MED  1
+#define QUALITY_LOW  2
 
 @interface PreferencesController : NSObject <NSWindowDelegate> {
-  /* General */
-  IBOutlet NSButton *scrobble;
-  IBOutlet NSButton *scrobbleLikes;
-  IBOutlet NSButton *scrobbleOnlyLiked;
-  IBOutlet NSButton *bindMedia;
-  IBOutlet NSButton *growl;
-  IBOutlet NSButton *growlPlayPause;
-  IBOutlet NSButton *growlNewSongs;
-
-  /* Playback */
-  IBOutlet NSButtonCell *highQuality;
-  IBOutlet NSButtonCell *mediumQuality;
-  IBOutlet NSButtonCell *lowQuality;
-
   IBOutlet NSToolbar *toolbar;
   IBOutlet NSView *general;
   IBOutlet NSView *playback;
   IBOutlet NSWindow *window;
 }
-
-/* General */
-- (IBAction) changeScrobbleTo: (id) sender;
-- (IBAction) changeScrobbleLikesTo: (id) sender;
-- (IBAction) changeScrobbleOnlyLikedTo: (id) sender;
-- (IBAction) changeBindMediaTo: (id) sender;
-- (IBAction) changeGrowlTo: (id) sender;
-- (IBAction) changeGrowlPlayPauseTo: (id) sender;
-- (IBAction) changeGrowlNewSongTo: (id) sender;
-
-/* Playback */
-- (IBAction) changeQualityToLow:(id)sender;
-- (IBAction) changeQualityToMedium:(id)sender;
-- (IBAction) changeQualityToHigh:(id)sender;
 
 - (IBAction) showGeneral: (id) sender;
 - (IBAction) showPlayback: (id) sender;
--- Classes/PreferencesController.m
+++ Classes/PreferencesController.m
@@ -6,35 +6,7 @@
 @implementation PreferencesController
 
 - (void)windowDidBecomeMain:(NSNotification *)notification {
-  NSInteger state;
   NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-
-  state = [defaults boolForKey:PLEASE_SCROBBLE] ? NSOnState : NSOffState;
-  [scrobble setState:state];
-  state = [defaults boolForKey:PLEASE_SCROBBLE_LIKES] ? NSOnState : NSOffState;
-  [scrobbleLikes setState:state];
-  state = [defaults boolForKey:ONLY_SCROBBLE_LIKED] ? NSOnState : NSOffState;
-  [scrobbleOnlyLiked setState:state];
-  state = [defaults boolForKey:PLEASE_BIND_MEDIA] ? NSOnState : NSOffState;
-  [bindMedia setState:state];
-  state = [defaults boolForKey:PLEASE_GROWL] ? NSOnState : NSOffState;
-  [growl setState:state];
-  state = [defaults boolForKey:PLEASE_GROWL_NEW] ? NSOnState : NSOffState;
-  [growlNewSongs setState:state];
-  state = [defaults boolForKey:PLEASE_GROWL_PLAY] ? NSOnState : NSOffState;
-  [growlPlayPause setState:state];
-
-  NSString *quality = [defaults objectForKey:DESIRED_QUALITY];
-  [highQuality setState:NSOffState];
-  [mediumQuality setState:NSOffState];
-  [lowQuality setState:NSOffState];
-  if ([quality isEqualToString:QUALITY_HIGH]) {
-    [highQuality setState:NSOnState];
-  } else if ([quality isEqualToString:QUALITY_LOW]) {
-    [lowQuality setState:NSOnState];
-  } else {
-    [mediumQuality setState:NSOnState];
-  }
 
   if ([[defaults objectForKey:LAST_PREF_PANE] isEqual:@"playback"]) {
     [self setPreferenceView:playback as:@"playback"];
@@ -65,91 +37,6 @@
 
   NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
   [defaults setObject:name forKey:LAST_PREF_PANE];
-}
-
-- (IBAction) changeScrobbleTo: (id) sender {
-  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-  BOOL selected = ([scrobble state] == NSOnState);
-
-  [defaults setBool:selected forKey:PLEASE_SCROBBLE];
-  [scrobbleLikes setEnabled:selected];
-  [scrobbleOnlyLiked setEnabled:selected];
-
-  if (selected) {
-    [Scrobbler subscribe];
-  } else {
-    [Scrobbler unsubscribe];
-  }
-}
-
-- (IBAction) changeScrobbleLikesTo: (id) sender {
-  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-
-  [defaults setBool:([scrobbleLikes state] == NSOnState)
-             forKey:PLEASE_SCROBBLE_LIKES];
-}
-
-- (IBAction) changeScrobbleOnlyLikedTo: (id) sender {
-  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-
-  [defaults setBool:([scrobbleOnlyLiked state] == NSOnState)
-             forKey:ONLY_SCROBBLE_LIKED];
-}
-
-- (IBAction) changeBindMediaTo: (id) sender {
-  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-
-  if ([bindMedia state] == NSOnState) {
-    [defaults setBool:YES forKey:PLEASE_BIND_MEDIA];
-    [AppleMediaKeyController bindKeys];
-  } else {
-    [defaults setBool:NO forKey:PLEASE_BIND_MEDIA];
-    [AppleMediaKeyController unbindKeys];
-  }
-}
-
-- (IBAction) changeGrowlTo: (id) sender {
-  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-  BOOL selected = ([growl state] == NSOnState);
-
-  [defaults setBool:selected forKey:PLEASE_GROWL];
-  [growlNewSongs setEnabled:selected];
-  [growlPlayPause setEnabled:selected];
-
-  if (selected) {
-    [Growler subscribe];
-  } else {
-    [Growler unsubscribe];
-  }
-}
-
-- (IBAction) changeGrowlPlayPauseTo:(id)sender {
-  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-
-  [defaults setBool:([growlPlayPause state] == NSOnState)
-             forKey:PLEASE_GROWL_PLAY];
-}
-
-- (IBAction) changeGrowlNewSongTo:(id)sender {
-  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-
-  [defaults setBool:([growlNewSongs state] == NSOnState)
-             forKey:PLEASE_GROWL_NEW];
-}
-
-- (IBAction) changeQualityToLow:(id)sender {
-  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-  [defaults setObject:QUALITY_LOW forKey:DESIRED_QUALITY];
-}
-
-- (IBAction) changeQualityToMedium:(id)sender {
-  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-  [defaults setObject:QUALITY_MED forKey:DESIRED_QUALITY];
-}
-
-- (IBAction) changeQualityToHigh:(id)sender {
-  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-  [defaults setObject:QUALITY_HIGH forKey:DESIRED_QUALITY];
 }
 
 - (IBAction) showGeneral: (id) sender {
