95a874eea6b71db14228b5d2fae4f6b282827c32
Alex Crichton <alex@alexcrichton.com>
2012-06-14 21:26:18 -0700

    Changes to AudioStreamer having moved to another project

--- AudioStreamer/AudioStreamer.m
+++ AudioStreamer/AudioStreamer.m
@@ -25,10 +25,13 @@
    Alex Crichton for the Hermes project */
 
 #import "AudioStreamer.h"
-#import "URLConnection.h"
 
 #define BitRateEstimationMaxPackets 5000
 #define BitRateEstimationMinPackets 50
+
+#define PROXY_SYSTEM 0
+#define PROXY_SOCKS  1
+#define PROXY_HTTP   2
 
 #define CHECK_ERR(err, code) {                                                 \
     if (err) { [self failWithErrorCode:code]; return; }                        \
@@ -45,6 +48,98 @@ typedef struct queued_packet {
 
 NSString * const ASStatusChangedNotification = @"ASStatusChangedNotification";
 
+/**
+ * @brief Class for streaming audio over an HTTP stream
+ *
+ * This class is implemented on top of Apple's AudioQueue framework. This
+ * framework is much too low-level for must use cases, so this class
+ * encapsulates the functionality to provide a nicer interface. The interface
+ * still requires some management, but it is far more sane than dealing with the
+ * AudioQueue structures yourself.
+ *
+ * This class is essentially a pipeline of three components to get audio to the
+ * speakers:
+ *
+ *      CFReadStream -> AudioFileStream -> AudioQueue
+ *
+ * CFReadStream:
+ *  The method of reading HTTP data is using the low-level CFReadStream class
+ *  because it allows configuration of proxies and scheduling/rescheduling on the
+ *  event loop. All data read from the HTTP stream is piped into the
+ *  AudioFileStream which then parses all of the data. This stage of the
+ *  pipeline also flags that events are happening to prevent a timeout. All
+ *  network activity occurs on the thread which started the audio stream.
+ *
+ * AudioFileStream:
+ *  This stage is implemented by Apple frameworks, and parses all audio data. It
+ *  is composed of two callbacks which receive data. The first callback invoked
+ *  in series is one which is notified whenever a new property is known about
+ *  the audio stream being received. Once all properties have been read, the
+ *  second callback beings to be invoked, and this callback is responsible for
+ *  dealing with packets.
+ *
+ *  The second callback is invoked whenever complete "audio packets" are
+ *  available to send to the audio queue. This stage is invoked on the call
+ *  stack of the stream which received the data (synchronously with receiving
+ *  the data).
+ *
+ *  Packets received are buffered in a static set of buffers allocated by the
+ *  audio queue instance. When a buffer is full, it is committed to the audio
+ *  queue, and then the next buffer is moved on to. Multiple packets can
+ *  possibly fit in one buffer. When committing a buffer, if there are no more
+ *  buffers available, then the http read stream is unscheduled from the run
+ *  loop and all currently received data is stored aside for later processing.
+ *
+ * AudioQueue:
+ *  This final stage is also implemented by Apple, and receives all of the full
+ *  buffers of data from the AudioFileStream's parsed packets. The
+ *  implementation manages its own set of threads, and callbacks are invoked on
+ *  the internal threads, not the main thread. The two callbacks that the audio
+ *  stream is interested in are playback state changing and audio buffers being
+ *  freed. In both cases, a message is queued for delivery on the main thread to
+ *  prevent synchronization issues.
+ *
+ *  When a buffer is freed, then it is marked as so, and if the stream was
+ *  waiting for a buffer to be freed a message to empty the queue as much as
+ *  possible is sent to the main thread's run loop. Otherwise no extra action
+ *  need be performed.
+ *
+ *  The main purpose of knowing when the playback state changes is to change the
+ *  state of the player accordingly.
+ *
+ * =============================================================================
+ *
+ * Errors
+ *  There are a large number of places where error can happen, and the stream
+ *  can bail out at any time with an error. Each error has its own code and
+ *  corresponding string representation. Any error will halt the entire audio
+ *  stream and cease playback.
+ *
+ *  Some errors might want to be handled by the manager of the AudioStreamer
+ *  class, but others normally indicate that the remote stream just won't work.
+ *  Occasionally errors might reflect a lack of local resources.
+ *
+ * =============================================================================
+ *
+ * Seeking
+ *  To seek inside an audio stream, the bit rate must be known along with some
+ *  other metadata, but this is not known until after the stream has started.
+ *  For this reason the seek can fail if not enough data is known yet.
+ *
+ *  If a seek succeeds, however, the actual method of doing so is as follows.
+ *  First, open a stream at position 0 and collect data about the stream, when
+ *  the seek is requested, cancel the stream and re-open the connection with the
+ *  proper byte offset. This second stream is then used to put data through the
+ *  pipelines.
+ *
+ * =============================================================================
+ *
+ * General notes
+ *  An audio stream is a one-shot thing. Once initialized, the source cannot be
+ *  changed and a single audio stream cannot be re-used. To do this, multiple
+ *  AudioStreamer objects need to be created/managed.
+ *
+ */
 @interface AudioStreamer ()
 
 - (void)handlePropertyChangeForFileStream:(AudioFileStreamID)inAudioFileStream
@@ -59,11 +154,18 @@ NSString * const ASStatusChangedNotification = @"ASStatusChangedNotification";
 - (void)handlePropertyChangeForQueue:(AudioQueueRef)inAQ
                           propertyID:(AudioQueuePropertyID)inID;
 
-- (int)enqueueBuffer;
 - (void)handleReadFromStream:(CFReadStreamRef)aStream
                    eventType:(CFStreamEventType)eventType;
 
 @end
+
+/* Woohoo, actual implementation now! */
+@implementation AudioStreamer
+
+@synthesize errorCode;
+@synthesize networkError;
+@synthesize state = state_;
+@synthesize httpHeaders;
 
 /* AudioFileStream callback when properties are available */
 void MyPropertyListenerProc(void *inClientData,
@@ -110,12 +212,6 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
   [streamer handleReadFromStream:aStream eventType:eventType];
 }
 
-@implementation AudioStreamer
-
-@synthesize errorCode;
-@synthesize networkError;
-@synthesize state = state_;
-@synthesize httpHeaders;
 
 //
 // initWithURL
@@ -135,6 +231,30 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
 //
 - (void)dealloc {
   [self stop];
+}
+
+/**
+ * @brief Set an HTTP proxy for this stream
+ *
+ * @param host the address/hostname of the remote host
+ * @param port the port of the proxy
+ */
+- (void) setHTTPProxy:(NSString*)host port:(int)port {
+  proxyHost = host;
+  proxyPort = port;
+  proxyType = PROXY_HTTP;
+}
+
+/**
+ * @brief Set SOCKS proxy for this stream
+ *
+ * @param host the address/hostname of the remote host
+ * @param port the port of the proxy
+ */
+- (void) setSOCKSProxy:(NSString*)host port:(int)port {
+  proxyHost = host;
+  proxyPort = port;
+  proxyType = PROXY_SOCKS;
 }
 
 /**
@@ -370,7 +490,36 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
     return NO;
   }
 
-  [URLConnection setHermesProxy:stream];
+  /* Deal with proxies */
+  switch (proxyType) {
+    case PROXY_HTTP: {
+      CFDictionaryRef proxySettings = (__bridge CFDictionaryRef)
+        [NSMutableDictionary dictionaryWithObjectsAndKeys:
+          proxyHost, kCFStreamPropertyHTTPProxyHost,
+          [NSNumber numberWithInt:proxyPort], kCFStreamPropertyHTTPProxyPort,
+          nil];
+      CFReadStreamSetProperty(stream, kCFStreamPropertyHTTPProxy,
+                              proxySettings);
+      break;
+    }
+    case PROXY_SOCKS: {
+      CFDictionaryRef proxySettings = (__bridge CFDictionaryRef)
+        [NSMutableDictionary dictionaryWithObjectsAndKeys:
+          proxyHost, kCFStreamPropertySOCKSProxyHost,
+          [NSNumber numberWithInt:proxyPort], kCFStreamPropertySOCKSProxyPort,
+          nil];
+      CFReadStreamSetProperty(stream, kCFStreamPropertySOCKSProxy,
+                              proxySettings);
+      break;
+    }
+    default:
+    case PROXY_SYSTEM: {
+      CFDictionaryRef proxySettings = CFNetworkCopySystemProxySettings();
+      CFReadStreamSetProperty(stream, kCFStreamPropertyHTTPProxy, proxySettings);
+      CFRelease(proxySettings);
+      break;
+    }
+  }
 
   /* handle SSL connections */
   if ([[url absoluteString] rangeOfString:@"https"].location == 0) {
@@ -415,11 +564,15 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
  * @brief Starts playback of this audio stream.
  *
  * This method can only be invoked once, and other methods will not work before
- * this method has been invoked
+ * this method has been invoked. All properties (like proxies) must be set
+ * before this method is invoked.
+ *
+ * @return YES if the stream was started, or NO if the stream was previously
+ *         started and this had no effect.
  */
-- (void) start {
+- (BOOL) start {
+  if (stream != NULL) return NO;
   assert(audioQueue == NULL);
-  assert(stream == NULL);
   assert(state_ == AS_INITIALIZED);
   [self openReadStream];
   timeout = [NSTimer scheduledTimerWithTimeInterval:2
@@ -427,6 +580,7 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
                                            selector:@selector(checkTimeout)
                                            userInfo:nil
                                             repeats:YES];
+  return YES;
 }
 
 /**
@@ -434,6 +588,8 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
  *        situation
  */
 - (void) checkTimeout {
+  /* Ignore if we're in the paused state */
+  if (state_ == AS_PAUSED) return;
   /* If the read stream has been unscheduled and not rescheduled, then this tick
      is irrelevant because we're not trying to read data anyway */
   if (unscheduled && !rescheduled) return;
@@ -471,7 +627,11 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
  *         enough information available to it to seek to the specified time.
  */
 - (BOOL)seekToTime:(double)newSeekTime {
-  if ([self calculatedBitRate] == 0.0 || fileLength <= 0) {
+  double bitrate;
+  double duration;
+  if (![self calculatedBitRate:&bitrate]) return NO;
+  if (![self duration:&duration]) return NO;
+  if (bitrate == 0.0 || fileLength <= 0) {
     return NO;
   }
 
@@ -479,7 +639,7 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
   // Calculate the byte offset for seeking
   //
   seekByteOffset = dataOffset +
-    (newSeekTime / self.duration) * (fileLength - dataOffset);
+    (newSeekTime / duration) * (fileLength - dataOffset);
 
   //
   // Attempt to leave 1 useful packet at the end of the file (although in
@@ -498,16 +658,15 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
   //
   // Attempt to align the seek with a packet boundary
   //
-  double calculatedBitRate = [self calculatedBitRate];
   double packetDuration = asbd.mFramesPerPacket / asbd.mSampleRate;
-  if (packetDuration > 0 && calculatedBitRate > 0) {
+  if (packetDuration > 0 && bitrate > 0) {
     UInt32 ioFlags = 0;
     SInt64 packetAlignedByteOffset;
     SInt64 seekPacket = floor(newSeekTime / packetDuration);
     err = AudioFileStreamSeek(audioFileStream, seekPacket,
                               &packetAlignedByteOffset, &ioFlags);
     if (!err && !(ioFlags & kAudioFileStreamSeekFlag_OffsetIsEstimated)) {
-      seekTime -= ((seekByteOffset - dataOffset) - packetAlignedByteOffset) * 8.0 / calculatedBitRate;
+      seekTime -= ((seekByteOffset - dataOffset) - packetAlignedByteOffset) * 8.0 / bitrate;
       seekByteOffset = packetAlignedByteOffset + dataOffset;
     }
   }
@@ -525,22 +684,27 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
   return [self openReadStream];
 }
 
-//
-// progress
-//
-// returns the current playback progress. Will return zero if sampleRate has
-// not yet been detected.
-//
-- (double)progress {
+/**
+ * @brief Calculate the progress into the stream, in seconds
+ *
+ * The AudioQueue instance is polled to determine the current time into the
+ * stream, and this is returned.
+ *
+ * @param ret a double which is filled in with the progress of the stream. The
+ *        contents are undefined if NO is returned.
+ * @return YES if the progress of the stream was determined, or NO if the
+ *         progress could not be determined at this time.
+ */
+- (BOOL) progress:(double*)ret {
   double sampleRate = asbd.mSampleRate;
   if (sampleRate <= 0 || (state_ != AS_PLAYING && state_ != AS_PAUSED))
-    return lastProgress;
+    return NO;
 
   AudioTimeStamp queueTime;
   Boolean discontinuity;
   err = AudioQueueGetCurrentTime(audioQueue, NULL, &queueTime, &discontinuity);
   if (err) {
-    return lastProgress;
+    return NO;
   }
 
   double progress = seekTime + queueTime.mSampleTime / sampleRate;
@@ -549,17 +713,22 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
   }
 
   lastProgress = progress;
-  return progress;
+  *ret = progress;
+  return YES;
 }
 
-//
-// calculatedBitRate
-//
-// returns the bit rate, if known. Uses packet duration times running bits per
-//   packet if available, otherwise it returns the nominal bitrate. Will return
-//   zero if no useful option available.
-//
-- (double)calculatedBitRate {
+/**
+ * @brief Calculates the bit rate of the stream
+ *
+ * All packets received so far contribute to the calculation of the bit rate.
+ * This is used internally to determine other factors like duration and
+ * progress.
+ *
+ * @param ret the double to fill in with the bit rate on success.
+ * @return YES if the bit rate could be calculated with a high degree of
+ *         certainty, or NO if it could not be.
+ */
+- (BOOL) calculatedBitRate:(double*)rate {
   double sampleRate     = asbd.mSampleRate;
   double packetDuration = asbd.mFramesPerPacket / sampleRate;
 
@@ -567,88 +736,94 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
     double averagePacketByteSize = processedPacketsSizeTotal /
                                     processedPacketsCount;
     /* bits/byte x bytes/packet x packets/sec = bits/sec */
-    return 8 * averagePacketByteSize / packetDuration;
+    *rate = 8 * averagePacketByteSize / packetDuration;
+    return YES;
   }
 
-  return 0;
+  return NO;
 }
 
-//
-// duration
-//
-// Calculates the duration of available audio from the bitRate and fileLength.
-//
-// returns the calculated duration in seconds.
-//
-- (double)duration {
-  double calculatedBitRate = [self calculatedBitRate];
-
+/**
+ * @brief Calculates the duration of the audio stream in seconds
+ *
+ * Uses information about the size of the file and the calculated bit rate to
+ * determine the duration of the stream.
+ *
+ * @param ret where to fill in with the duration of the stream on success.
+ * @return YES if ret contains the duration of the stream, or NO if the duration
+ *         could not be determined. In the NO case, the contents of ret are
+ *         undefined
+ */
+- (BOOL) duration:(double*)ret {
+  double calculatedBitRate;
+  if (![self calculatedBitRate:&calculatedBitRate]) return NO;
   if (calculatedBitRate == 0 || fileLength == 0) {
-    return 0.0;
+    return NO;
   }
 
-  return (fileLength - dataOffset) / (calculatedBitRate * 0.125);
+  *ret = (fileLength - dataOffset) / (calculatedBitRate * 0.125);
+  return YES;
 }
 
-//
-// pause
-//
-// Pause the stream if it's playing
-//
-- (void)pause {
+/**
+ * @brief Pause the audio stream if playing
+ *
+ * @return YES if the audio stream was paused, or NO if it was not in the
+ *         AS_PLAYING state or an error occurred.
+ */
+- (BOOL) pause {
+  if (state_ != AS_PLAYING) return NO;
   assert(audioQueue != NULL);
-  if (state_ == AS_PLAYING) {
-    err = AudioQueuePause(audioQueue);
-    CHECK_ERR(err, AS_AUDIO_QUEUE_PAUSE_FAILED);
-    [self setState:AS_PAUSED];
+  err = AudioQueuePause(audioQueue);
+  if (err) {
+    [self failWithErrorCode:AS_AUDIO_QUEUE_PAUSE_FAILED];
+    return NO;
   }
+  [self setState:AS_PAUSED];
+  return YES;
 }
 
-//
-// play
-//
-// Play the stream if it's not playing
-//
-- (void) play {
+/**
+ * @brief Plays the audio stream if paused
+ *
+ * @return YES if the audio stream entered into the AS_PLAYING state, or NO if
+ *         any other error or bad state was encountered.
+ */
+- (BOOL) play {
+  if (state_ != AS_PAUSED) return NO;
   assert(audioQueue != NULL);
-  if (state_ == AS_PAUSED) {
-    err = AudioQueueStart(audioQueue, NULL);
-    CHECK_ERR(err, AS_AUDIO_QUEUE_START_FAILED);
-    [self setState:AS_PLAYING];
+  err = AudioQueueStart(audioQueue, NULL);
+  if (err) {
+    [self failWithErrorCode:AS_AUDIO_QUEUE_START_FAILED];
+    return NO;
   }
+  [self setState:AS_PLAYING];
+  return YES;
 }
 
-//
-// stop
-//
-// This method can be called to stop downloading/playback before it completes.
-// It is automatically called when an error occurs.
-//
-// If playback has not started before this method is called, it will toggle the
-// "isPlaying" property so that it is guaranteed to transition to true and
-// back to false
-//
-- (void)stop {
+/**
+ * @brief Stop all streams, cleaning up resources and preventing all further
+ *        events from ocurring.
+ *
+ * This method may be invoked at any time from any point of the audio stream as
+ * a signal of error happening. This method sets the state to AS_STOPPED if it
+ * isn't already AS_STOPPED or AS_DONE.
+ */
+- (void) stop {
   if (![self isDone]) {
     [self setState:AS_STOPPED];
   }
 
-  [self closeReadStream];
   [timeout invalidate];
   timeout = nil;
 
-  //
-  // Close the audio file strea,
-  //
+  /* Clean up our streams */
+  [self closeReadStream];
   if (audioFileStream) {
     err = AudioFileStreamClose(audioFileStream);
     assert(!err);
     audioFileStream = nil;
   }
-
-  //
-  // Dispose of the Audio Queue
-  //
   if (audioQueue) {
     AudioQueueStop(audioQueue, true);
     err = AudioQueueDispose(audioQueue, true);
@@ -1066,7 +1241,7 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
 
   /* Place each packet into a buffer and then send each buffer into the audio
      queue */
-  int i;
+  UInt32 i;
   for (i = 0; i < inNumberPackets && !waitingOnBuffer && queued_head == NULL; i++) {
     AudioStreamPacketDescription *desc = &inPacketDescriptions[i];
     int ret = [self handlePacket:(inInputData + desc->mStartOffset)
@@ -1101,7 +1276,7 @@ void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType,
 - (int) handlePacket:(const void*)data
                 desc:(AudioStreamPacketDescription*)desc{
   assert(audioQueue != NULL);
-  SInt64 packetSize = desc->mDataByteSize;
+  UInt64 packetSize = desc->mDataByteSize;
 
   /* This shouldn't happen because most of the time we read the packet buffer
      size from the file stream, but if we restored to guessing it we could
