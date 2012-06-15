8604e3dc8b9f0bf25c295da6649f63e22a1adb11
Alex Crichton <alex@alexcrichton.com>
2012-06-14 21:26:18 -0700

    Changes to AudioStreamer having moved to another project

--- AudioStreamer/AudioStreamer.h
+++ AudioStreamer/AudioStreamer.h
@@ -27,30 +27,14 @@
 #import <Cocoa/Cocoa.h>
 #include <AudioToolbox/AudioToolbox.h>
 
-/* TODO: don't have this, buffer entire stream */
-#define kNumAQBufs 16      // Number of audio queue buffers we allocate.
-// Needs to be big enough to keep audio pipeline
-// busy (non-zero number of queued buffers) but
-// not so big that audio takes too long to begin
-// (kNumAQBufs * kAQBufSize of data must be
-// loaded before playback will start).
-//
-// Set LOG_QUEUED_BUFFERS to 1 to log how many
-// buffers are queued at any time -- if it drops
-// to zero too often, this value may need to
-// increase. Min 3, typical 8-24.
+/* Number of buffers, should phase out soon... */
+#define kNumAQBufs 16
 
-#define kAQDefaultBufSize 2048  // Number of bytes in each audio queue buffer
-// Needs to be big enough to hold a packet of
-// audio from the audio file. If number is too
-// large, queuing of audio before playback starts
-// will take too long.
-// Highly compressed files can use smaller
-// numbers (512 or less). 2048 should hold all
-// but the largest packets. A buffer size error
-// will occur if this number is too small.
+/* Needs to be big enough to hold one packet of audio */
+#define kAQDefaultBufSize 2048
 
-#define kAQMaxPacketDescs 512  // Number of packet descriptions in our array
+/* Maximum number of packets which can be contained in one buffer */
+#define kAQMaxPacketDescs 512
 
 typedef enum {
   AS_INITIALIZED = 0,
@@ -94,101 +78,14 @@ extern NSString * const ASStatusChangedNotification;
 
 struct queued_packet;
 
-/**
- * @brief Class for streaming audio over an HTTP stream
- *
- * This class is implemented on top of Apple's AudioQueue framework. This
- * framework is much too low-level for must use cases, so this class
- * encapsulates the functionality to provide a nicer interface. The interface
- * still requires some management, but it is far more sane than dealing with the
- * AudioQueue structures yourself.
- *
- * This class is essentially a pipeline of three components to get audio to the
- * speakers:
- *
- *      CFReadStream -> AudioFileStream -> AudioQueue
- *
- * CFReadStream:
- *  The method of reading HTTP data is using the low-level CFReadStream class
- *  because it allows configuration of proxies and scheduling/rescheduling on the
- *  event loop. All data read from the HTTP stream is piped into the
- *  AudioFileStream which then parses all of the data. This stage of the
- *  pipeline also flags that events are happening to prevent a timeout. All
- *  network activity occurs on the thread which started the audio stream.
- *
- * AudioFileStream:
- *  This stage is implemented by Apple frameworks, and parses all audio data. It
- *  is composed of two callbacks which receive data. The first callback invoked
- *  in series is one which is notified whenever a new property is known about
- *  the audio stream being received. Once all properties have been read, the
- *  second callback beings to be invoked, and this callback is responsible for
- *  dealing with packets.
- *
- *  The second callback is invoked whenever complete "audio packets" are
- *  available to send to the audio queue. This stage is invoked on the call
- *  stack of the stream which received the data (synchronously with receiving
- *  the data).
- *
- *  Packets received are buffered in a static set of buffers allocated by the
- *  audio queue instance. When a buffer is full, it is committed to the audio
- *  queue, and then the next buffer is moved on to. Multiple packets can
- *  possibly fit in one buffer. When committing a buffer, if there are no more
- *  buffers available, then the http read stream is unscheduled from the run
- *  loop and all currently received data is stored aside for later processing.
- *
- * AudioQueue:
- *  This final stage is also implemented by Apple, and receives all of the full
- *  buffers of data from the AudioFileStream's parsed packets. The
- *  implementation manages its own set of threads, and callbacks are invoked on
- *  the internal threads, not the main thread. The two callbacks that the audio
- *  stream is interested in are playback state changing and audio buffers being
- *  freed. In both cases, a message is queued for delivery on the main thread to
- *  prevent synchronization issues.
- *
- *  When a buffer is freed, then it is marked as so, and if the stream was
- *  waiting for a buffer to be freed a message to empty the queue as much as
- *  possible is sent to the main thread's run loop. Otherwise no extra action
- *  need be performed.
- *
- *  The main purpose of knowing when the playback state changes is to change the
- *  state of the player accordingly.
- *
- * =============================================================================
- *
- * Errors
- *  There are a large number of places where error can happen, and the stream
- *  can bail out at any time with an error. Each error has its own code and
- *  corresponding string representation. Any error will halt the entire audio
- *  stream and cease playback.
- *
- *  Some errors might want to be handled by the manager of the AudioStreamer
- *  class, but others normally indicate that the remote stream just won't work.
- *  Occasionally errors might reflect a lack of local resources.
- *
- * =============================================================================
- *
- * Seeking
- *  To seek inside an audio stream, the bit rate must be known along with some
- *  other metadata, but this is not known until after the stream has started.
- *  For this reason the seek can fail if not enough data is known yet.
- *
- *  If a seek succeeds, however, the actual method of doing so is as follows.
- *  First, open a stream at position 0 and collect data about the stream, when
- *  the seek is requested, cancel the stream and re-open the connection with the
- *  proper byte offset. This second stream is then used to put data through the
- *  pipelines.
- *
- * =============================================================================
- *
- * General notes
- *  An audio stream is a one-shot thing. Once initialized, the source cannot be
- *  changed and a single audio stream cannot be re-used. To do this, multiple
- *  AudioStreamer objects need to be created/managed.
- *
- */
+/* Compact description of what the AudioStreamer can do, for a detailed
+   description of all methods, see the source, AudioStreamer.m */
 @interface AudioStreamer : NSObject {
   /* Properties specified at creation */
   NSURL *url;
+  int proxyType;
+  NSString *proxyHost;
+  int proxyPort;
 
   /* Creates as part of the [start] method */
   CFReadStreamRef stream;
@@ -253,21 +150,28 @@ struct queued_packet;
 @property (readonly) NSDictionary *httpHeaders;
 @property (readonly) NSError *networkError;
 
-+ (NSString *)stringForErrorCode:(AudioStreamerErrorCode)anErrorCode;
++ (NSString*) stringForErrorCode:(AudioStreamerErrorCode)anErrorCode;
 
-- (id)initWithURL:(NSURL *)aURL;
-- (void)start;
-- (void)stop;
-- (void)pause;
-- (void)play;
-- (BOOL)isPlaying;
-- (BOOL)isPaused;
-- (BOOL)isWaiting;
-- (BOOL)isDone;
-- (BOOL)seekToTime:(double)newSeekTime;
-- (double)calculatedBitRate;
-- (BOOL)setVolume:(double)volume;
-- (double)duration;
-- (double)progress;
+/* Creating an audio stream and managing properties before starting */
+- (AudioStreamer*) initWithURL:(NSURL*)url;
+- (void) setHTTPProxy:(NSString*)host port:(int)port;
+- (void) setSOCKSProxy:(NSString*)host port:(int)port;
+
+/* Management of the stream and testing state */
+- (BOOL) start;
+- (void) stop;
+- (BOOL) pause;
+- (BOOL) play;
+- (BOOL) isPlaying;
+- (BOOL) isPaused;
+- (BOOL) isWaiting;
+- (BOOL) isDone;
+
+/* Calculated properties and modifying the stream (all can fail) */
+- (BOOL) seekToTime:(double)newSeekTime;
+- (BOOL) calculatedBitRate:(double*)ret;
+- (BOOL) setVolume:(double)volume;
+- (BOOL) duration:(double*)ret;
+- (BOOL) progress:(double*)ret;
 
 @end
