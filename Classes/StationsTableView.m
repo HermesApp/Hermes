275389ff624eb513e068d99d36c67193a7d8675c
Winston Weinert <winston@ml1.net>
2014-01-01 02:30:14 -0600

    Pressing space now always toggles playback, even with the History Drawer or Stations Drawer visible.

--- Classes/StationsTableView.m
+++ Classes/StationsTableView.m
@@ -8,14 +8,19 @@
 
 #import "StationsTableView.h"
 #import "StationsController.h"
+#import "HermesAppDelegate.h"
+#import "PlaybackController.h"
 
 @implementation StationsTableView
 
 - (void)keyDown:(NSEvent *)theEvent {
-  if ([[theEvent characters] isEqualToString:@"\r"])
+  if ([[theEvent characters] isEqualToString:@"\r"]) {
     [playButton performClick:self];
-  else
+  } else if ([[theEvent characters] isEqualToString:@" "]) {
+    [[[NSApp delegate] playback] playpause:nil];
+  } else {
     [super keyDown:theEvent];
+  }
 }
 
 @end
