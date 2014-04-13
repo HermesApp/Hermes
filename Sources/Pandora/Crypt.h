84c338dbc491be46e00f90cc1dd3a0b0981d799d
Winston Weinert <winston@ml1.net>
2014-04-12 21:11:18 -0500

    Clean up Crypt.[mh] documentation and style

--- Sources/Pandora/Crypt.h
+++ Sources/Pandora/Crypt.h
@@ -1,7 +1,30 @@
+/**
+ * @file Pandora/Crypt.h
+ * @brief Implementation of the encryption/decryption of requests to/from
+ *        Pandora
+ *
+ * The encryption algorithm used is Blowfish in ECB mode.
+ */
+
 #ifndef CRYPT_H
 #define CRYPT_H
 
+/**
+ * @brief Encrypt some data for Pandora
+ *
+ * @param data the data to encrypt
+ * @param encryptionKey the encryption key to use
+ * @return the encrypted data, hex encoded
+ */
 NSData* PandoraEncryptData(NSData* string, NSString *encryptionKey);
+
+/**
+ * @brief Decrypt some data received from Pandora
+ *
+ * @param string the hex-encoded string to be decrypted.
+ * @param decryptionKey the decryption key to use
+ * @return the decrypted data
+ */
 NSData* PandoraDecryptString(NSString* string, NSString *decryptionKey);
 
 #endif /* CRYPT_H */
--- Sources/Pandora/Crypt.m
+++ Sources/Pandora/Crypt.m
@@ -1,15 +1,6 @@
-/**
- * @file Pandora/Crypt.m
- * @brief Implementation of the encryption/decryption of requests to/from
- *        Pandora
- *
- * Currently the encryption algorithm used is Blowfish in ECB mode.
- */
-
 #include "blowfish/blowfish.h"
 
 #import "Crypt.h"
-#import "Pandora.h"
 
 /* Conversion from hex to int and int to hex */
 static char i2h[16] = "0123456789abcdef";
@@ -20,38 +11,30 @@ static char h2i[256] = {
 };
 
 static void appendByte(unsigned char byte, void *_data) {
-  NSMutableData *data = (__bridge NSMutableData*) _data;
+  NSMutableData *data = (__bridge NSMutableData *)_data;
   [data appendBytes:&byte length:1];
 }
 
 static void appendHex(unsigned char byte, void *_data) {
-  NSMutableData *data = (__bridge NSMutableData*) _data;
+  NSMutableData *data = (__bridge NSMutableData *)_data;
   char bytes[2];
   bytes[1] = i2h[byte % 16];
   bytes[0] = i2h[byte / 16];
   [data appendBytes:bytes length:2];
 }
 
-/**
- * @brief Decrypt some data received from Pandora
- *
- * @param string the string which should be decrypted. The string should also be
- *               hex encoded
- * @return the decrypted data
- */
-NSData* PandoraDecryptString(NSString* string, NSString *decryptionKey) {
+NSData* PandoraDecryptString(NSString *string, NSString *decryptionKey) {
   struct blf_ecb_ctx ctx;
   NSMutableData *mut = [[NSMutableData alloc] init];
   const char *key = decryptionKey.UTF8String;
   
-  Blowfish_ecb_start(&ctx, FALSE, (unsigned char *) key,
+  Blowfish_ecb_start(&ctx, FALSE, (unsigned char *)key,
                      sizeof(key) - 1, appendByte,
-                     (__bridge void *) mut);
+                     (__bridge void *)mut);
 
   const char *bytes = [string cStringUsingEncoding:NSASCIIStringEncoding];
   int len = [string lengthOfBytesUsingEncoding:NSASCIIStringEncoding];
-  int i;
-  for (i = 0; i < len; i += 2) {
+  for (int i = 0; i < len; i += 2) {
     Blowfish_ecb_feed(&ctx, h2i[(int) bytes[i]] * 16 + h2i[(int) bytes[i + 1]]);
   }
   Blowfish_ecb_stop(&ctx);
@@ -59,25 +42,18 @@ NSData* PandoraDecryptString(NSString* string, NSString *decryptionKey) {
   return mut;
 }
 
-/**
- * @brief Encrypt some data for Pandora
- *
- * @param data the data to encrypt
- * @return the encrypted data, hex encoded
- */
-NSData* PandoraEncryptData(NSData* data, NSString *encryptionKey) {
+NSData* PandoraEncryptData(NSData *data, NSString *encryptionKey) {
   struct blf_ecb_ctx ctx;
   NSMutableData *mut = [[NSMutableData alloc] init];
   const char *key = encryptionKey.UTF8String;
 
-  Blowfish_ecb_start(&ctx, TRUE, (unsigned char*) key,
+  Blowfish_ecb_start(&ctx, TRUE, (unsigned char*)key,
                      sizeof(key) - 1, appendHex,
-                     (__bridge void*) mut);
+                     (__bridge void*)mut);
 
   const char *bytes = [data bytes];
   int len = [data length];
-  int i;
-  for (i = 0; i < len; i++) {
+  for (int i = 0; i < len; i++) {
     Blowfish_ecb_feed(&ctx, bytes[i]);
   }
   Blowfish_ecb_stop(&ctx);
