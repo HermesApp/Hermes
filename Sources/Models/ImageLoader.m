1dab2588b6891e5ec12d312cc0379af6a429c8a8
Winston Weinert <winston@ml1.net>
2014-06-06 19:49:59 -0500

    Log if ImageLoader url is cached or not
    Also remove queueing message.

--- Sources/Models/ImageLoader.m
+++ Sources/Models/ImageLoader.m
@@ -23,7 +23,6 @@
   if (cur != nil) {
     [queue addObject:url];
     [cbqueue addObject:cb];
-    NSLogd(@"queueing %@", url);
     return;
   }
 
@@ -31,10 +30,10 @@
 }
 
 - (void) fetch:(NSString*)url cb:(ImageCallback)cb {
-  NSLogd(@"fetching: %@", url);
   NSURLRequest *req = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
   cur = [URLConnection connectionForRequest:req
                           completionHandler:^(NSData *d, NSError *error) {
+    NSLogd(@"fetching: %@", url);
     cb(d);
     cur = nil;
     curURL = nil;
@@ -42,6 +41,7 @@
     /* If any pending requests are to this url, also satisfy them */
     NSUInteger idx;
     while ((idx = [queue indexOfObject:url]) != NSNotFound) {
+      NSLogd(@"cached: %@", url);
       [queue removeObjectAtIndex:idx];
       ImageCallback cb = cbqueue[idx];
       cb(d);
