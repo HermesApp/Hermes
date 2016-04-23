8421c58427a860511fb0782f73854f0b21b4e43a
Nicholas Riley <com-github@sabi.net>
2016-04-23 17:58:48 -0400

    Use current system font for credits in about box.

--- Sources/HermesApp.m
+++ Sources/HermesApp.m
@@ -11,10 +11,27 @@
 @implementation HermesApp
 
 - (void)orderFrontStandardAboutPanelWithOptions:(NSDictionary *)optionsDictionary {
-  // XXX work around bug in OS X 10.7 / 10.8 where the Credits text is not centered (r. 14829080)
+  // XXX work around bug in OS X 10.7–10.11 where the Credits text is not centered (r. 14829080)
   NSSet *windowsBefore = [NSSet setWithArray:[NSApp windows]];
 
-  [super orderFrontStandardAboutPanelWithOptions:optionsDictionary];
+  // change credits font to current system font
+  NSData *creditsData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Credits" ofType:@"rtf"]];
+  NSMutableAttributedString *credits = [[NSMutableAttributedString alloc] initWithRTF:creditsData documentAttributes:nil];
+  NSString *systemFontFamily = [[NSFont systemFontOfSize:[NSFont labelFontSize]].fontDescriptor objectForKey:NSFontFamilyAttribute];
+
+  NSFontManager *fontManager = [NSFontManager sharedFontManager];
+  NSRange effectiveRange = {0, 0};
+  NSUInteger length = credits.length;
+  while (NSMaxRange(effectiveRange) < length) {
+    NSFont *font = [credits attribute:NSFontAttributeName atIndex:NSMaxRange(effectiveRange) effectiveRange:&effectiveRange];
+    font = [fontManager convertFont:font toFamily:systemFontFamily];
+    [credits addAttribute:NSFontAttributeName value:font range:effectiveRange];
+  }
+
+  NSMutableDictionary *optionsWithCredits = optionsDictionary == nil ? [[NSMutableDictionary alloc] initWithCapacity:1] : [optionsDictionary mutableCopy];
+  optionsWithCredits[@"Credits"] = credits;
+
+  [super orderFrontStandardAboutPanelWithOptions:optionsWithCredits];
 
   for (NSWindow *window in [NSApp windows]) {
     if ([windowsBefore containsObject:window])
