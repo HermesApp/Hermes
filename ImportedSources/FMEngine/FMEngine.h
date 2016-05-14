b1f47fe3a40fe83bab78333059d64ded85631e05
Nicholas Riley <com-github@sabi.net>
2016-05-14 15:16:16 -0400

    Scrobble securely.

--- ImportedSources/FMEngine/FMEngine.h
+++ ImportedSources/FMEngine/FMEngine.h
@@ -11,7 +11,7 @@
 
 #define _LASTFM_API_KEY_ @"31fc44bcd6e21954afb404d179a09e9a"
 #define _LASTFM_SECRETK_ @"a146429ed54f25b8bf9d5ca3cc423260"
-#define _LASTFM_BASEURL_ @"http://ws.audioscrobbler.com/2.0/"
+#define _LASTFM_BASEURL_ @"https://ws.audioscrobbler.com/2.0/"
 
 // Comment the next line to use XML
 #define _USE_JSON_ 1
