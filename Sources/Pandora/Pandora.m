HermesApp/Hermes

commit 5f898cf0b95c978d808ca7f6c1435a5867ac773f

Add support for high quality 192k mp3

Sources/Pandora/Pandora.m
@@ -480,20 +480,32 @@ static NSString *hierrs[] = {
       [song setArtistUrl: s[@"artistDetailUrl"]];
       [song setTitleUrl: s[@"songDetailUrl"]];
       
+      id audioUrlMap = s[@"audioUrlMap"];
+      if ([audioUrlMap isKindOfClass:[NSDictionary class]]) {
+        if ([audioUrlMap[@"highQuality"] isKindOfClass:[NSDictionary class]])
+             [song setHighUrl:audioUrlMap[@"highQuality"][@"audioUrl"]];
+        if ([audioUrlMap[@"mediumQuality"] isKindOfClass:[NSDictionary class]])
+             [song setMedUrl:audioUrlMap[@"mediumQuality"][@"audioUrl"]];
+      }
+      
       id urls = s[@"additionalAudioUrl"];
       if ([urls isKindOfClass:[NSArray class]]) {
         NSArray *urlArray = urls;
         [song setLowUrl:urlArray[0]];
         if ([urlArray count] > 1) {
-          [song setMedUrl:urlArray[1]];
+          if (![song medUrl])
+            [song setMedUrl:urlArray[1]];
         } else {
-          [song setMedUrl:[song lowUrl]];
+          if (![song medUrl])
+            [song setMedUrl:[song lowUrl]];
           NSLog(@"bad medium format specified in request");
         }
         if ([urlArray count] > 2) {
-          [song setHighUrl:urlArray[2]];
+          if (![song highUrl])
+            [song setHighUrl:urlArray[2]];
         } else {
-          [song setHighUrl:[song medUrl]];
+          if (![song highUrl])
+            [song setHighUrl:[song medUrl]];
           NSLog(@"bad high format specified in request");
         }
       } else {
@@ -547,7 +559,7 @@ static NSString *hierrs[] = {
     created.year = 2000 + [dateCreated[@"year"] integerValue];
     created.month = 1 + [dateCreated[@"month"] integerValue];
     created.day = [dateCreated[@"date"] integerValue];
-    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
     info[@"created"] = [gregorian dateFromComponents:created];
     NSString *art = result[@"artUrl"];
     if (art != nil) { info[@"art"] = art; }

commit 59f095cee88292481f7716000b1dd28e7797e1b3

Second try at audio quality mapping (#263).
Fixes the regression introduced in 5f898cf0b95c978d808ca7f6c1435a5867ac773f for non-Pandora One users (which introduced decoding of audioUrlMap) where they were getting 128 Kbps MP3s and in 1.2.7 got 64 Kbps AAC+ instead, while at the same time *improving* audio quality for Pandora One users from 128 Kbps to 192 Kbps MP3.

The comments as to what Pandora returns for each quality level reflect my testing today, but of course may change without warning in future.  Using bitrate as a crude comparator, we try at least not to degrade quality by using audioUrlMap.

Sources/Pandora/Pandora.m
@@ -480,21 +480,7 @@ static NSString *hierrs[] = {
       song.albumUrl = s[@"albumDetailUrl"];
       song.artistUrl = s[@"artistDetailUrl"];
       song.titleUrl = s[@"songDetailUrl"];
-      
-      id audioUrlMap = s[@"audioUrlMap"];
-      if ([audioUrlMap isKindOfClass:[NSDictionary class]]) {
-        id qualityMap = audioUrlMap[@"highQuality"];
-        if ([qualityMap isKindOfClass:[NSDictionary class]]) {
-          song.highUrl = qualityMap[@"audioUrl"]; // 192 Kbps MP3 with Pandora One
-          NSLogd(@"High quality audio from audioUrlMap is %@ Kbps %@", qualityMap[@"bitrate"], qualityMap[@"encoding"]);
-        }
-        qualityMap = audioUrlMap[@"mediumQuality"];
-        if ([qualityMap isKindOfClass:[NSDictionary class]]) {
-          song.medUrl = audioUrlMap[@"mediumQuality"][@"audioUrl"]; // 64 Kbps AAC+ with Pandora One
-          NSLogd(@"Medium quality audio from audioUrlMap is %@ Kbps %@", qualityMap[@"bitrate"], qualityMap[@"encoding"]);
-        }
-      }
-      
+
       id urls = s[@"additionalAudioUrl"];
       if ([urls isKindOfClass:[NSArray class]]) {
         NSArray *urlArray = urls;
@@ -502,18 +488,37 @@ static NSString *hierrs[] = {
           NSLog(@"Fewer than 3 (expected) items for additionalAudioUrl: %@", urlArray);
         }
         switch (urlArray.count) {
-          case 3:
-            if (!song.highUrl) song.highUrl = urlArray[2];
-          case 2:
-            if (!song.medUrl) song.medUrl = urlArray[1];
-          case 1:
-            if (!song.lowUrl) song.lowUrl = urlArray[0];
+          case 3: song.highUrl = urlArray[2];
+          case 2: song.medUrl = urlArray[1];
+          case 1: song.lowUrl = urlArray[0];
             break;
           default:
             NSLog(@"Unexpected number of items (not 1-3) for additionalAudioUrl: %@", urlArray);
         }
       } else {
         NSLog(@"Unexpected format for additionalAudioUrl: %@", urls);
+      }
+
+      id audioUrlMap = s[@"audioUrlMap"];
+      if ([audioUrlMap isKindOfClass:[NSDictionary class]]) {
+        id qualityMap = audioUrlMap[@"highQuality"];
+        if ([qualityMap isKindOfClass:[NSDictionary class]]) {
+          NSLogd(@"High quality audio from audioUrlMap is %@ Kbps %@", qualityMap[@"bitrate"], qualityMap[@"encoding"]);
+          if (!song.highUrl || [qualityMap[@"bitrate"] integerValue] > 128)
+            song.highUrl = qualityMap[@"audioUrl"]; // 192 Kbps MP3 with Pandora One; 64 Kbps AAC+ without
+        }
+        qualityMap = audioUrlMap[@"mediumQuality"];
+        if ([qualityMap isKindOfClass:[NSDictionary class]]) {
+          NSLogd(@"Medium quality audio from audioUrlMap is %@ Kbps %@", qualityMap[@"bitrate"], qualityMap[@"encoding"]);
+          if (!song.medUrl || [qualityMap[@"bitrate"] integerValue] > 64)
+            song.medUrl = qualityMap[@"audioUrl"]; // 64 Kbps AAC+
+        }
+        qualityMap = audioUrlMap[@"lowQuality"];
+        if ([qualityMap isKindOfClass:[NSDictionary class]]) {
+          NSLogd(@"Low quality audio from audioUrlMap is %@ Kbps %@", qualityMap[@"bitrate"], qualityMap[@"encoding"]);
+          if (!song.lowUrl || [qualityMap[@"bitrate"] integerValue] > 32)
+            song.lowUrl = qualityMap[@"audioUrl"]; // 32 Kbps AAC+ (not provided without Pandora One)
+        }
       }
 
       if (!song.medUrl) song.medUrl = song.lowUrl;