6658721301a2458683e4f37c67cf5a902d5a1f5d
Nicholas Riley <com-github@sabi.net>
2016-09-09 08:48:25 -0400

    Only fix drawer background on 10.10 (and get the color right!)

--- Sources/Views/HermesBackgroundView.m
+++ Sources/Views/HermesBackgroundView.m
@@ -11,9 +11,12 @@
 @implementation HermesBackgroundView
 
 - (void)drawRect:(NSRect)dirtyRect {
-  // work around ugly drawer background - doesn't seem to match any of the standard Apple system colors...
-  [[NSColor colorWithGenericGamma22White:0.95 alpha:1] setFill];
-  NSRectFill(dirtyRect);
+  // work around ugly drawer background on 10.10
+  // - doesn't seem to match any of the standard Apple system colors...
+  if (NSAppKitVersionNumber <= NSAppKitVersionNumber10_10_Max) {
+    [[NSColor colorWithGenericGamma22White:241/255. alpha:1] setFill];
+    NSRectFill(dirtyRect);
+  }
 }
 
 @end
