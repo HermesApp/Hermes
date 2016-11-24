HermesApp/Hermes

commit 3fadb0ba357449c73e9bf8689f917dca331b02e6

modernize code a bit

Sources/Pandora/Pandora.m
@@ -296,7 +296,7 @@ static NSString *hierrs[] = {
                         @"password":    self.device[kPandoraDevicePassword],
                         @"deviceModel": self.device[kPandoraDeviceDeviceID],
                         @"version":     PANDORA_API_VERSION,
-                        @"includeUrls": [NSNumber numberWithBool:TRUE]
+                        @"includeUrls": @TRUE
                         };
   request.method    = @"auth.partnerLogin";
   request.encrypted = FALSE;
@@ -320,7 +320,7 @@ static NSString *hierrs[] = {
     NSNumber *subscriberStatus = respDict[@"result"][@"isSubscriber"];
     if (subscriberStatus == nil) {
       NSLogd(@"Warning: no key isSubscriber, assuming non-subscriber.");
-      self.cachedSubscriberStatus = [NSNumber numberWithBool:NO];
+      self.cachedSubscriberStatus = @NO;
     } else {
       self.cachedSubscriberStatus = subscriberStatus;
     }
@@ -565,7 +565,7 @@ static NSString *hierrs[] = {
 - (BOOL) fetchStationInfo:(Station *)station {
   NSMutableDictionary *d = [self defaultRequestDictionary];
   d[@"stationToken"] = [station token];
-  d[@"includeExtendedAttributes"] = [NSNumber numberWithBool:TRUE];
+  d[@"includeExtendedAttributes"] = @TRUE;
   
   PandoraRequest *req = [self defaultRequestWithMethod:@"station.getStation"];
   [req setRequest:d];
@@ -704,7 +704,7 @@ static NSString *hierrs[] = {
   
   NSMutableDictionary *d = [self defaultRequestDictionary];
   d[@"stationToken"] = [[song station] token];
-  d[@"includeExtendedAttributes"] = [NSNumber numberWithBool:TRUE];
+  d[@"includeExtendedAttributes"] = @TRUE;
   
   PandoraRequest *req = [self defaultRequestWithMethod:@"station.getStation"];
   [req setRequest:d];
@@ -741,7 +741,7 @@ static NSString *hierrs[] = {
 #pragma mark - syncTime
 
 - (NSNumber*) syncTimeNum {
-  return [NSNumber numberWithLongLong: sync_time + ([self time] - start_time)];
+  return @(sync_time + ([self time] - start_time));
 }
 
 - (int64_t) time {

Hermes.xcodeproj/xcshareddata/xcschemes/Hermes.xcscheme
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <Scheme
-   LastUpgradeVersion = "0800"
+   LastUpgradeVersion = "0810"
    version = "1.3">
    <BuildAction
       parallelizeBuildables = "YES"

Sources/URLConnection.m
@@ -176,12 +176,12 @@ static void URLConnectionStreamCallback(CFReadStreamRef aStream,
                  host:(NSString*)host
                  port:(NSInteger)port {
   CFDictionaryRef proxySettings = (__bridge CFDictionaryRef)
-    [NSMutableDictionary dictionaryWithObjectsAndKeys:
-      host, kCFStreamPropertyHTTPProxyHost,
-      [NSNumber numberWithInteger:port], kCFStreamPropertyHTTPProxyPort,
-      host, kCFStreamPropertyHTTPSProxyHost,
-      [NSNumber numberWithInteger:port], kCFStreamPropertyHTTPSProxyPort,
-      nil];
+          [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                  host, kCFStreamPropertyHTTPProxyHost,
+                  @(port), kCFStreamPropertyHTTPProxyPort,
+                  host, kCFStreamPropertyHTTPSProxyHost,
+                  @(port), kCFStreamPropertyHTTPSProxyPort,
+                          nil];
   CFReadStreamSetProperty(stream, kCFStreamPropertyHTTPProxy, proxySettings);
 }
 
@@ -189,10 +189,10 @@ static void URLConnectionStreamCallback(CFReadStreamRef aStream,
                  host:(NSString*)host
                  port:(NSInteger)port {
   CFDictionaryRef proxySettings = (__bridge CFDictionaryRef)
-    [NSMutableDictionary dictionaryWithObjectsAndKeys:
-      host, kCFStreamPropertySOCKSProxyHost,
-      [NSNumber numberWithInteger:port], kCFStreamPropertySOCKSProxyPort,
-      nil];
+          [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                  host, kCFStreamPropertySOCKSProxyHost,
+                  @(port), kCFStreamPropertySOCKSProxyPort,
+                          nil];
   CFReadStreamSetProperty(stream, kCFStreamPropertySOCKSProxy, proxySettings);
 }
 

commit 42784f6553873f695d21099ace1d1fa894ebce74

Fix regression in Pandora One login introduced in 3fadb0ba357449c73e9bf8689f917dca331b02e6.
Note that @TRUE is not the same as [NSNumber numberWithBool:TRUE].  Objective-C prefers @YES/@NO in any case.

You can see the difference with the code below:

  NSData *data;
  data = [NSPropertyListSerialization dataWithPropertyList:@{@"includeUrls": @TRUE} format:NSPropertyListXMLFormat_v1_0 options:0 error:NULL];
  NSLog(@"%@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);

  data = [NSPropertyListSerialization dataWithPropertyList:@{@"includeUrls": @YES} format:NSPropertyListXMLFormat_v1_0 options:0 error:NULL];
  NSLog(@"%@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);

  data = [NSPropertyListSerialization dataWithPropertyList:@{@"includeUrls": [NSNumber numberWithBool:TRUE]} format:NSPropertyListXMLFormat_v1_0 options:0 error:NULL];
  NSLog(@"%@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);

Output:
2016-11-24 13:15:18.545 Hermes[76608:8805098] <?xml version="1.0" encoding="UTF-8"?>
<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
<plist version="1.0">
<dict>
	<key>includeUrls</key>
	<integer>1</integer>
</dict>
</plist>
2016-11-24 13:15:18.545 Hermes[76608:8805098] <?xml version="1.0" encoding="UTF-8"?>
<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
<plist version="1.0">
<dict>
	<key>includeUrls</key>
	<true/>
</dict>
</plist>
2016-11-24 13:15:18.545 Hermes[76608:8805098] <?xml version="1.0" encoding="UTF-8"?>
<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
<plist version="1.0">
<dict>
	<key>includeUrls</key>
	<true/>
</dict>
</plist>

Sources/Pandora/Pandora.m
@@ -291,7 +291,7 @@ static NSString *hierrs[] = {
                         @"password":    self.device[kPandoraDevicePassword],
                         @"deviceModel": self.device[kPandoraDeviceDeviceID],
                         @"version":     PANDORA_API_VERSION,
-                        @"includeUrls": @TRUE
+                        @"includeUrls": @YES
                         };
   request.method    = @"auth.partnerLogin";
   request.encrypted = FALSE;
@@ -560,7 +560,7 @@ static NSString *hierrs[] = {
 - (BOOL) fetchStationInfo:(Station *)station {
   NSMutableDictionary *d = [self defaultRequestDictionary];
   d[@"stationToken"] = [station token];
-  d[@"includeExtendedAttributes"] = @TRUE;
+  d[@"includeExtendedAttributes"] = @YES;
   
   PandoraRequest *req = [self defaultRequestWithMethod:@"station.getStation"];
   [req setRequest:d];
@@ -699,7 +699,7 @@ static NSString *hierrs[] = {
   
   NSMutableDictionary *d = [self defaultRequestDictionary];
   d[@"stationToken"] = [[song station] token];
-  d[@"includeExtendedAttributes"] = @TRUE;
+  d[@"includeExtendedAttributes"] = @YES;
   
   PandoraRequest *req = [self defaultRequestWithMethod:@"station.getStation"];
   [req setRequest:d];