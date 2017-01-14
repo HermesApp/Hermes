e501f6af9faee2b1645f1e442fabf03cb955a803
Nicholas Riley <com-github@sabi.net>
2017-01-14 18:26:15 -0500

    Credits text still not centered after 6 OS versions.
    Quality!

--- Sources/HermesApp.m
+++ Sources/HermesApp.m
@@ -11,7 +11,7 @@
 @implementation HermesApp
 
 - (void)orderFrontStandardAboutPanelWithOptions:(NSDictionary *)optionsDictionary {
-  // XXX work around bug in OS X 10.7–10.11 where the Credits text is not centered (r. 14829080)
+  // XXX work around bug in OS X 10.7–10.12 where the Credits text is not centered (r. 14829080)
   NSSet *windowsBefore = [NSSet setWithArray:[NSApp windows]];
 
   // change credits font to current system font
